0a461499a346bbb8101ff702b981f68a9add4a3d
Ando <takuya0411@gmail.com>
2015-12-14 14:29:22 +0900

    コメントをわかりやすくするために修正

--- FavHashtag.js
+++ FavHashtag.js
@@ -1,22 +1,21 @@
-// twitterモジュールを読み込み
 var twitter = require('twitter');
-// アプリ登録時に取得したkeyを入れてOAuth認証し、初期化
+
+//変更すべき設定
 var client = new twitter({
     consumer_key: 'consumer_key',
     consumer_secret: 'consumer_secret',
     access_token_key: 'access_token_key',
     access_token_secret: 'access_token_secret'
 });
-var tag = '#いいねした人全員フォローする';
+var tag = '検索したいハッシュタグ・単語';
+//変更すべき点はここまで
 
-// Public APIのstatuses/filterで取得したタイムラインを、自分のアカウント名を含む文字列でフィルターする
 client.stream( 'statuses/filter', { track : tag }, function( stream ) {
-    // フィルターされたデータのストリームを受け取り、ツイートのテキストを表示する
     stream.on( 'data', function( data ) {
-    	var tweetid = data.id_str; // ツイートのID
+    	var tweetid = data.id_str;
 		client.post('favorites/create',{id: tweetid},  function(error, tweet, response){
                       if(error) throw error;
-                      console.log(response);  // Raw response object.
+                      console.log(response);
               });
     	});
 });
